4d75682c7b0b5806c10974fa02813806358579bb
kavinranawella <kavin256@gmail.com>
2022-01-16 00:51:55 +0530

    shopping list completed commit

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-const Header: () => Node = () => {
+const Header = ({title}) => {
   return (
-    <View style={style.header}>
-      <Text style={style.text}>Shopping List</Text>
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
 
-const style = StyleSheet.create({
-  header: {height: 60, padding: 15, backgroundColor: 'darkslateblue'},
-  text: {color: '#fff', fontSize: 23, textAlign: 'center'},
+Header.defaultProps = {
+  title: 'Shoppinhg List',
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
+  },
+  text: {
+    color: 'white',
+    fontSize: 23,
+    textAlign: 'center',
+  },
 });
 
 export default Header;
